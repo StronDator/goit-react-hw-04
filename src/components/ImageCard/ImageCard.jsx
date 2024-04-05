62e9b9300b899c5b0f6818af1f49a9d71b8a1186
Andrian Mamchuk <andrian.mamchuk@gmail.com>
2024-04-05 17:54:11 +0300

    Fixed bugs ImageCard Component

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -2,19 +2,17 @@ import style from "./ImageCard.module.css";
 
 export default function ImageCard({ card, onSelect }) {
   return (
-    <div
-      className={style.wrapper}
-      onClick={() =>
-        onSelect(true, {
-          src: card.urls.regular,
-          description: card.alt_description,
-        })
-      }
-    >
+    <div className={style.wrapper}>
       <img
         className={style.img}
         src={card.urls.small}
         alt={card.alt_description}
+        onClick={() =>
+          onSelect(true, {
+            src: card.urls.regular,
+            description: card.alt_description,
+          })
+        }
       />
       <div className={style.overlay}>
         <p className={style.text}>Likes: {card.likes}</p>
